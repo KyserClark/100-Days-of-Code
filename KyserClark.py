5dbca049610bf9af31553ee72d27448673c96f5e
Kyser Clark <92742375+KyserClark@users.noreply.github.com>
2022-12-24 13:38:10 -0900

    Update KyserClark.py

--- KyserClark.py
+++ KyserClark.py
@@ -1,3 +1,3 @@
-# This code is just to make the repository a Python repository, to view my code, please go to my completed-projects repository
+# This code is just to make this repository a Python repository, to view my code, please go to my completed-projects repository.
 
-print("Hello World")
+print("KyserClark.com")
